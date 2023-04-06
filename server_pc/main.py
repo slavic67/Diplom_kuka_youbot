7e75a5ea871987f19303030fbe228254747b840b
unknown <arcan0n@protonmail.com>
2023-04-06 22:45:21 +0300

    the first working version of the server

--- server_pc/main.py
+++ server_pc/main.py
@@ -15,12 +15,14 @@ def tracking_object():
         img = cv2.flip(img, 1)
 
         start=time.time()
-        return_list=find_all_object(img)
+        temp=find_all_object(img)
+        print(temp)
+        if temp !=[]:
+            return_list=temp
         end=time.time()
 
         cv2.putText(img, "{} fps".format(round(1/(end-start),2)), (20,30), cv2.FONT_HERSHEY_PLAIN, 3, (0,0, 255), 2)
-        #if return_list!=[]:
-           # print(return_list[0])
+
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
@@ -32,13 +34,25 @@ def start_server(HOST):
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind(HOST)
     server.listen()
+    weight = 640
+    height = 480
+    # if aspect ratio 16:9
+    weight_diapason = 16 * 9.25
+    height_diapason = 9 * 9.25
+    # if aspect ratio 4:3
+    #weight_diapason = 4 * 34
+    #height_diapason = 3 * 34
+
+
     while True:
         conn,addr=server.accept()
         print('Connected from -',addr)
-        resp=pickle.dumps(return_list)
+        delta_angle=[(return_list[0][0]-weight//2)/weight*weight_diapason,(return_list[0][1]-height//2)/height*height_diapason]
+        return_list=[[weight//2,height//2]]
+        resp=pickle.dumps(delta_angle)
         conn.send(resp)
-        print(return_list)
         conn.close()
+
 
 
 if __name__ == '__main__':
@@ -47,11 +61,9 @@ if __name__ == '__main__':
     HOST = (socket.gethostname(), 10000)
     return_list = []
 
+
     thread1=threading.Thread(target=tracking_object)
     thread1.start()
     thread2=threading.Thread(target=start_server,kwargs={'HOST':HOST})
     thread2.start()
 
-    while True:
-        print(return_list)
-        time.sleep(0.1)
